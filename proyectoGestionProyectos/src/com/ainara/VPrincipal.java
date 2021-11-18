b2c1ec72152a4b71a1f56ed6b6dca2f092d5b0f6
Ainara <ainaragrajales@hotmail.com>
2021-11-18 21:22:06 +0100

    menu botones con submenu y subitems

---
+++ proyectoGestionProyectos/src/com/ainara/VPrincipal.java
@@ -0,0 +1,14 @@
+package com.ainara;
+
+import javax.swing.*;
+
+public class VPrincipal {
+    private JMenuBar menu_bar;
+    private JMenu menu1;
+    private JMenu menu2;
+    private JMenu menu3;
+    private JMenu subMenu2;
+    private JMenuItem sub_item1;
+    private JMenuItem sub_item2;
+    private JMenuItem item1;
+}
