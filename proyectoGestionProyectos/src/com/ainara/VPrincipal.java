e9450a22f9d32db02d8c76ca4120d297267c1c20
Ainara <ainaragrajales@hotmail.com>
2021-11-19 21:30:21 +0100

    creados actionListeners

--- proyectoGestionProyectos/src/com/ainara/VPrincipal.java
+++ proyectoGestionProyectos/src/com/ainara/VPrincipal.java
@@ -1,6 +1,7 @@
 package com.ainara;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -29,23 +30,110 @@ public class VPrincipal {
     private JMenuItem item_suministroProveedores_gestion;
     private JMenuItem item_suministrosPiezas_gestion;
     private JMenuItem item_estadisticas_gestion;
+    private JPanel VPanelPrincipal;
 
 
     public VPrincipal() {
-        menu_proveedores.addActionListener(e -> {
+
+
+
+        //Botones que llevan a las ventanas de Proveedores
+        item_gestion_proveedores.addActionListener(e -> {
+            JFrame frame_gestion_proveedores = new JFrame("Gestión de Proveedores");
+            frame_gestion_proveedores.setContentPane(new VGestionProveedores().getVPanelGestionProveedores());
+            frame_gestion_proveedores.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Cierra la ventana pero no el programa
+            frame_gestion_proveedores.setMinimumSize(new Dimension(400, 200)); // Lo ajusto a un tamaño para que se vea bien
+            frame_gestion_proveedores.setLocationRelativeTo(null); // Saca la ventana al centro
+            frame_gestion_proveedores.pack();
+            frame_gestion_proveedores.setVisible(true);
+        });
+
+        subitem_consultaCodigo_proveedores.addActionListener(e -> {
+            JFrame frame_consCod_proveedores = new JFrame("Consulta de Proveedores por Código");
+            frame_consCod_proveedores.setContentPane(new VConsultaCodigoProveedores().getVPanelConsCodProveedores());
+            frame_consCod_proveedores.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Cierra la ventana pero no el programa
+            frame_consCod_proveedores.setMinimumSize(new Dimension(400, 200)); // Lo ajusto a un tamaño para que se vea bien
+            frame_consCod_proveedores.setLocationRelativeTo(null); // Saca la ventana al centro
+            frame_consCod_proveedores.pack();
+            frame_consCod_proveedores.setVisible(true);
+        });
+
+        subitem_consultaNombre_proveedores.addActionListener(e -> {
+            JFrame frame_consNom_proveedores = new JFrame("Consulta de Proveedores por Nombre");
+            frame_consNom_proveedores.setContentPane(new VConsultaNombreProveedores().getVPanelConsNombProveedores());
+            frame_consNom_proveedores.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Cierra la ventana pero no el programa
+            frame_consNom_proveedores.setMinimumSize(new Dimension(400, 200)); // Lo ajusto a un tamaño para que se vea bien
+            frame_consNom_proveedores.setLocationRelativeTo(null); // Saca la ventana al centro
+            frame_consNom_proveedores.pack();
+            frame_consNom_proveedores.setVisible(true);
+        });
+
+        subitem_consultaDireccion_proveedores.addActionListener(e -> {
+            JFrame frame_consDir_proveedores = new JFrame("Consulta de Proveedores por Dirección");
+            frame_consDir_proveedores.setContentPane(new VConsultaDireccionProveedores().getVPanelConsDirProveedores());
+            frame_consDir_proveedores.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Cierra la ventana pero no el programa
+            frame_consDir_proveedores.setMinimumSize(new Dimension(400, 200)); // Lo ajusto a un tamaño para que se vea bien
+            frame_consDir_proveedores.setLocationRelativeTo(null); // Saca la ventana al centro
+            frame_consDir_proveedores.pack();
+            frame_consDir_proveedores.setVisible(true);
+        });
+
+
+        //Botones que llevan a las ventanas de Piezas
+        item_gestion_piezas.addActionListener(e -> {
 
         });
-        menu_piezas.addActionListener(e -> {
+
+        subitem_consultaCodigo_piezas.addActionListener(e -> {
 
         });
-        menu_proyectos.addActionListener(e -> {
+
+        subitem_consultaNombre_piezas.addActionListener(e -> {
 
         });
-        menu_gestion.addActionListener(e -> {
+
+        //Botones que llevan a las ventanas de Proyectos
+        item_gestion_proyectos.addActionListener(e -> {
 
         });
-        menu_ayuda.addActionListener(e -> {
+
+        subitem_consultaCodigo_proyectos.addActionListener(e -> {
 
         });
+
+        subitem_consultaNombre_proyectos.addActionListener(e -> {
+
+        });
+
+        subitem_consultaCiudad_proyectos.addActionListener(e -> {
+
+        });
+
+        //Botones que llevan a las ventanas de Gestión Global
+        item_relacionar_gestion.addActionListener(e -> {
+
+        });
+
+        item_suministroProveedores_gestion.addActionListener(e -> {
+
+        });
+
+        item_suministrosPiezas_gestion.addActionListener(e -> {
+
+        });
+
+        item_estadisticas_gestion.addActionListener(e -> {
+            
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Gestión de Proyectos");
+        frame.setContentPane(new VPrincipal().VPanelPrincipal);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMinimumSize(new Dimension(300, 300));
+        frame.setLocationRelativeTo(null); // Saca la ventana al centro
+        frame.pack();
+        frame.setVisible(true);
     }
 }
