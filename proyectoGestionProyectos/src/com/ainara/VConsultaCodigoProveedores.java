2ba81ba925595467109d62e586bc20fbf4952064
Ainara <ainaragrajales@hotmail.com>
2021-11-19 20:56:20 +0100

    ventana consulta codigo proveedores

---
+++ proyectoGestionProyectos/src/com/ainara/VConsultaCodigoProveedores.java
@@ -0,0 +1,10 @@
+package com.ainara;
+
+import javax.swing.*;
+
+public class VConsultaCodigoProveedores {
+    private JTextArea textArea1;
+    private JTextField textField1;
+    private JButton buscarProveedorButton;
+    private JComboBox comboBox1;
+}
