fdca8cddaadab5c214762707903f889d9fe590ac
Ainara <ainaragrajales@hotmail.com>
2021-11-19 19:53:07 +0100

    Todos los botones del menú creados

--- proyectoGestionProyectos/src/com/ainara/VPrincipal.java
+++ proyectoGestionProyectos/src/com/ainara/VPrincipal.java
@@ -4,11 +4,27 @@ import javax.swing.*;
 
 public class VPrincipal {
     private JMenuBar menu_bar;
-    private JMenu menu1;
-    private JMenu menu2;
-    private JMenu menu3;
-    private JMenu subMenu2;
-    private JMenuItem sub_item1;
-    private JMenuItem sub_item2;
-    private JMenuItem item1;
+    private JMenu menu_proveedores;
+    private JMenu menu_piezas;
+    private JMenu menu_proyectos;
+    private JMenu menu_gestion;
+    private JMenu menu_ayuda;
+    private JMenuItem item_gestion_proveedores;
+    private JMenu submenu_consultas_proveedores;
+    private JMenuItem subitem_consultaCodigo_proveedores;
+    private JMenuItem subitem_consultaNombre_proveedores;
+    private JMenuItem subitem_consultaDireccion_proveedores;
+    private JMenuItem item_gestion_piezas;
+    private JMenu submenu_consultas_piezas;
+    private JMenuItem subitem_consultaCodigo_piezas;
+    private JMenuItem subitem_consultaNombre_piezas;
+    private JMenuItem item_gestion_proyectos;
+    private JMenu submenu_consultas_proyectos;
+    private JMenuItem subitem_consultaCodigo_proyectos;
+    private JMenuItem subitem_consultaNombre_proyectos;
+    private JMenuItem subitem_consultaCiudad_proyectos;
+    private JMenuItem item_relacionar_gestion;
+    private JMenuItem item_suministroProveedores_gestion;
+    private JMenuItem item_suministrosPiezas_gestion;
+    private JMenuItem item_estadisticas_gestion;
 }
