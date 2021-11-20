e8fe2143acc751811452d50283f923c046f6c191
Ainara <ainaragrajales@hotmail.com>
2021-11-20 11:17:27 +0100

    nombres edit text

--- proyectoGestionProyectos/src/com/ainara/VGestionProveedores.java
+++ proyectoGestionProyectos/src/com/ainara/VGestionProveedores.java
@@ -7,24 +7,24 @@ public class VGestionProveedores {
     private JPanel VPanelGestionProveedores;
     private JPanel pestana_gestionProveedores;
     private JPanel pestana_listadoProveedores;
-    private JButton limpiarButton;
-    private JButton insertarButton;
-    private JButton modificarButton;
-    private JButton eliminarButton;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
-    private JTextField textField6;
-    private JTextField textField7;
-    private JTextField textField8;
-    private JButton ejecutarConsultaButton;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton bajaButton;
+    private JButton b_limpiar_proveedor;
+    private JButton b_insertar_proveedor;
+    private JButton b_modificar_proveedor;
+    private JButton b_eliminar_proveedor;
+    private JTextField et_cod_proveedor;
+    private JTextField et_nom_proveedor;
+    private JTextField et_ape_proveedor;
+    private JTextField et_dir_proveedor;
+    private JTextField et_codList_proveedor;
+    private JTextField et_nomList_proveedor;
+    private JTextField et_apeList_proveedor;
+    private JTextField et_dirList_proveedor;
+    private JButton b_ejecutarConsulta_proveedor;
+    private JButton b_first_proveedor;
+    private JButton b_prev_proveedor;
+    private JButton b_next_proveedor;
+    private JButton b_last_proveedor;
+    private JButton b_baja_proveedor;
 
     public VGestionProveedores(){
 
