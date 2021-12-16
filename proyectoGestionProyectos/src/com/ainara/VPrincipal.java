1921d6ebde83a23ea8994e3efdd6ae5b89d869b6
Ainara Grajales Rial <ainara.grajales@ikasle.egibide.org>
2021-12-16 23:18:42 +0100

    botones ventana principal

--- proyectoGestionProyectos/src/com/ainara/VPrincipal.java
+++ proyectoGestionProyectos/src/com/ainara/VPrincipal.java
@@ -81,32 +81,74 @@ public class VPrincipal {
 
         //Botones que llevan a las ventanas de Piezas
         item_gestion_piezas.addActionListener(e -> {
-
+            JFrame frame_gestion_piezas = new JFrame("Gestión de Piezas");
+            frame_gestion_piezas.setContentPane(new VGestionPiezas().getVPanelGestionPiezas());
+            frame_gestion_piezas.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Cierra la ventana pero no el programa
+            frame_gestion_piezas.setMinimumSize(new Dimension(400, 200)); // Lo ajusto a un tamaño para que se vea bien
+            frame_gestion_piezas.setLocationRelativeTo(null); // Saca la ventana al centro
+            frame_gestion_piezas.pack();
+            frame_gestion_piezas.setVisible(true);
         });
 
         subitem_consultaCodigo_piezas.addActionListener(e -> {
-
+            JFrame frame_consCod_piezas = new JFrame("Consulta de Piezas Por Código");
+            frame_consCod_piezas.setContentPane(new VConsultaCodigoPiezas().getVPanelConsCodPiezas());
+            frame_consCod_piezas.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Cierra la ventana pero no el programa
+            frame_consCod_piezas.setMinimumSize(new Dimension(400, 200)); // Lo ajusto a un tamaño para que se vea bien
+            frame_consCod_piezas.setLocationRelativeTo(null); // Saca la ventana al centro
+            frame_consCod_piezas.pack();
+            frame_consCod_piezas.setVisible(true);
         });
 
         subitem_consultaNombre_piezas.addActionListener(e -> {
-
+            JFrame frame_consNom_piezas = new JFrame("Consulta de Piezas Por Nombre");
+            frame_consNom_piezas.setContentPane(new VConsultaNombrePiezas().getVPanelConsNombPiezas());
+            frame_consNom_piezas.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Cierra la ventana pero no el programa
+            frame_consNom_piezas.setMinimumSize(new Dimension(400, 200)); // Lo ajusto a un tamaño para que se vea bien
+            frame_consNom_piezas.setLocationRelativeTo(null); // Saca la ventana al centro
+            frame_consNom_piezas.pack();
+            frame_consNom_piezas.setVisible(true);
         });
 
         //Botones que llevan a las ventanas de Proyectos
         item_gestion_proyectos.addActionListener(e -> {
-
+            JFrame frame_gestion_proyectos = new JFrame("Gestión de Proyectos");
+            frame_gestion_proyectos.setContentPane(new VGestionProyectos().getVPanelGestionProyectos());
+            frame_gestion_proyectos.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Cierra la ventana pero no el programa
+            frame_gestion_proyectos.setMinimumSize(new Dimension(400, 200)); // Lo ajusto a un tamaño para que se vea bien
+            frame_gestion_proyectos.setLocationRelativeTo(null); // Saca la ventana al centro
+            frame_gestion_proyectos.pack();
+            frame_gestion_proyectos.setVisible(true);
         });
 
         subitem_consultaCodigo_proyectos.addActionListener(e -> {
-
+            JFrame frame_consCod_proyectos = new JFrame("Consulta de Proyectos Por Código");
+            frame_consCod_proyectos.setContentPane(new VConsultaCodigoProyectos().getVPanelConsCodProyectos());
+            frame_consCod_proyectos.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Cierra la ventana pero no el programa
+            frame_consCod_proyectos.setMinimumSize(new Dimension(400, 200)); // Lo ajusto a un tamaño para que se vea bien
+            frame_consCod_proyectos.setLocationRelativeTo(null); // Saca la ventana al centro
+            frame_consCod_proyectos.pack();
+            frame_consCod_proyectos.setVisible(true);
         });
 
         subitem_consultaNombre_proyectos.addActionListener(e -> {
-
+            JFrame frame_consNom_proyectos = new JFrame("Consulta de Proyectos Por Nombre");
+            frame_consNom_proyectos.setContentPane(new VConsultaNombreProyectos().getVPanelConsNombProyectos());
+            frame_consNom_proyectos.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Cierra la ventana pero no el programa
+            frame_consNom_proyectos.setMinimumSize(new Dimension(400, 200)); // Lo ajusto a un tamaño para que se vea bien
+            frame_consNom_proyectos.setLocationRelativeTo(null); // Saca la ventana al centro
+            frame_consNom_proyectos.pack();
+            frame_consNom_proyectos.setVisible(true);
         });
 
         subitem_consultaCiudad_proyectos.addActionListener(e -> {
-
+            JFrame frame_consCiud_proyectos = new JFrame("Consulta de Proyectos Por Ciudad");
+            frame_consCiud_proyectos.setContentPane(new VConsultaCiudadProyectos().getVPanelConsCiudProyectos());
+            frame_consCiud_proyectos.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Cierra la ventana pero no el programa
+            frame_consCiud_proyectos.setMinimumSize(new Dimension(400, 200)); // Lo ajusto a un tamaño para que se vea bien
+            frame_consCiud_proyectos.setLocationRelativeTo(null); // Saca la ventana al centro
+            frame_consCiud_proyectos.pack();
+            frame_consCiud_proyectos.setVisible(true);
         });
 
         //Botones que llevan a las ventanas de Gestión Global
